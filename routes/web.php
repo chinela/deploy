f020bd21e932bd445b47479afd49cc55e201a626
chinedu chiaju <chineduchiaju@gmail.com>
2020-11-27 21:03:00 +0100

    testing queue worker on digital ocean

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/queue', function () {
+    Mail::to('chinedu@gmai.commm')->send(new TestMail());
+    return 'working';
 });
